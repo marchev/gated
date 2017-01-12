a0cf8a4b00642b11c1b8436293bbc9d5ed111763
Martin Marchev <n1tmare@gmail.com>
2017-01-12 22:22:35 +0200

    Initial commit

---
+++ src/main/java/org/marchev/GatedApplication.java
@@ -0,0 +1,12 @@
+package org.marchev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatedApplication.class, args);
+	}
+}
