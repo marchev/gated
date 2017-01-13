48a33227fd059af4cea22f2bff4810e27b0e4f54
Martin Marchev <n1tmare@gmail.com>
2017-01-13 22:42:43 +0200

    Refactoring: import organization

--- src/main/java/org/marchev/gated/model/Apartment.java
+++ src/main/java/org/marchev/gated/model/Apartment.java
@@ -1,6 +1,10 @@
 package org.marchev.gated.model;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Apartment {
