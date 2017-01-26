9b8369b69f287cd2610a9016881da107d16c5784
Martin Marchev <n1tmare@gmail.com>
2017-01-26 23:32:51 +0200

    UI layout changes:
    - Add toolbar
    - Add sidenav with menu items
    - Add search button
    - Add user menu

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -1,5 +1,15 @@
 import Vue from 'vue'
+import VueMaterial from 'vue-material'
 import App from './App.vue'
+
+Vue.use(VueMaterial)
+
+Vue.material.registerTheme('default', {
+  primary: 'blue',
+  accent: 'pink',
+  warn: 'deep-orange',
+  background: 'white'
+})
 
 new Vue({
   el: '#app',
