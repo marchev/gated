c388b0110a2b016d549ec360d3ccbd5e1ae9fd8d
Martin Marchev <n1tmare@gmail.com>
2017-02-12 15:50:29 +0200

    Remove UI artifacts from the API project

--- src/main/java/org/marchev/gated/controller/WebController.java
+++
@@ -1,8 +0,0 @@
-package org.marchev.gated.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-public class WebController {
-}
